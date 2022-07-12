7c0f3d40e875058dd1493d1d1ccd27c458fc6755
medkov77 <medkov77@mail.rum>
2022-07-12 20:53:35 +0300

    task1

---
+++ notes.controller.js
@@ -0,0 +1,54 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+
+  await saveNotes(notes);
+  console.log(chalk.bgGreen("Note was added!"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.bgWhite(note.id), chalk.blue(note.title));
+  });
+}
+async function removeNote(id) {
+  const notes = await getNotes();
+  const index = notes.findIndex((note) => note.id === id);
+  if (index === -1) {
+    console.log(chalk.bgRed(`Note with id ${id} not found`));
+  } else {
+    notes.splice(index, 1);
+    console.log(chalk.bgBlue(`Note with id ${id} wasgit deleted`));
+  }
+
+  saveNotes(notes);
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
